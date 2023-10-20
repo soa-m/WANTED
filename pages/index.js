2cf12db32158fa8325649d1a538d6d330054e994
Kosuke <kosuke.mega@gmail.com>
2023-10-20 21:42:55 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1 className="{styles.title}">Hello Next.js!</h1>
+      <h1 className={styles.heading}>Hello Next.js!</h1>
       <ul>
         <li>
           <Link href="/about">
