ea197eae02df46573bac65408e85c28c6c3c86f1
Kosuke <kosuke.mega@gmail.com>
2023-10-20 16:44:37 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="{styles.title}">Hello Next.js!</h1>
+      <ul>
+        <li>
+          <Link href="/about">
+            about
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
