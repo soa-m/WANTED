8c9c6955bf61507d0648e26a6531696eb78c8ebc
rukoruko9 <shimotsu96@gmail.com>
2023-10-22 02:55:59 +0900

    firstBranch

--- components/header.js
+++ components/header.js
@@ -2,18 +2,5 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Header() {
-  return (
-    <ul>
-      <li>
-        <Link href="/">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          About
-        </Link>
-      </li>
-    </ul>
-  );
+
 }
