5028b58e8d2f38e6c22f99fff84a7a3caf0fba52
Kosuke <kosuke.mega@gmail.com>
2023-10-30 04:56:32 +0900

    てがみ

--- pages/Serch.js
+++ pages/Serch.js
@@ -1,67 +1,153 @@
-import { useState } from "react";
+import { useRef,useState,useEffect } from "react"
+import React from "react";
 import styles from '../styles/Home.module.css';
-import {SearchData} from '../components/MainProgram';
+import {SearchData,FirstItems} from '../components/MainProgram';
+import Link from 'next/link';
+import jsQR from "jsqr";
 export default function Home() {
 
-
+ 
     //サーチボタンが押されたらテキストボックスの値に対応する返しを行う。該当する返しがない場合は "検索した内容"に関連する内容はみつかりませんでしたと表示。 
     // textは検索ボックス内の内容
     //SearchedWordは検索ボタンが押されたときの検索ボックス内の内容
     //ReturnWord1は表示する返しの言葉を格納
-    var [text, setText] = useState("");
-    var [ReturnWord1,setReturnWord1] = useState("");
+    
     function OnSearch(){
-        var ReturnWord; //返する言葉 
-        var SearchData_keys = Object.keys(SearchData)
-        var check1 = false;
-        SearchData_keys.find(function(value){
-            if(value == text){
-                ReturnWord = SearchData[text];
-                check1 = true;
-                
-            
-            }
-        }
-        )
+        var SearchedWord = document.getElementById("SearchBox").value;
+        console.log(SearchedWord);
+        var SearchData_keys = Object.keys(SearchData);
+        var ReturnWord = SearchData_keys.find(function(value) {
+            return value  == SearchedWord;
+        })
         
-         if(check1 == false) ReturnWord = text + "に関する内容は見つかりませんでした";
-         setReturnWord1(ReturnWord);
-         
+        if(ReturnWord == undefined) ReturnWord =  SearchedWord + "に関する内容は見つかりませんでした";
+        else ReturnWord = FirstSearchData[SearchedWord];
+        document.getElementById("SearchResult").innerHTML = ReturnWord;
+        console.log(ReturnWord);
 
     };
+    var clickednum  = 0;
+    function a(){
+       clickednum++;
+    const video = document.createElement('video');
+      const canvasElement = document.getElementById('canvas');
+      const canvas = canvasElement.getContext('2d');
+      const loading = document.getElementById('loading');
+      let isReadQR = false;
+      
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+        .then((stream) => {
+          video.srcObject = stream;
+          video.setAttribute('playsinline', true);
+          video.play();
+          requestAnimationFrame(tick);
 
-
+        });
+      
+      function tick() {
+        loading.textContent = '⌛ ロード中...';
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+          loading.hidden = true;
+          canvasElement.hidden = false;
+          canvasElement.height = video.videoHeight;
+          canvasElement.width = video.videoWidth;
+          canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+          var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+          var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: 'dontInvert',
+          });
+          if (code && !isReadQR) {
+            document.getElementById('qr-msg').textContent = `QRコード：${code.data}`;
+            isReadQR = false;
+          }
+        }
+        if(clickednum % 2 == 1){
+            requestAnimationFrame(tick);
+            document.getElementById("camerabutton").innerHTML = "カメラを停止する";
+      }else  {
+        canvasElement.hidden = true;
+        document.getElementById("camerabutton").innerHTML = "カメラを起動する";
+      }    
+    }
+        
+      }
     return (
+        
+        
       <div>
         
-        <a href="/Information" className={styles.btnInformation}>　情報　</a>
-        <a href="/Serch" className={styles.btnSearch}>　検索　</a>
-        <a href="/Relations" className={styles.btnRelations}>　関係図　</a>
-        <a href="/Missions" className={styles.btnMissions}>ミッション</a>
-        <a href="/PastInformation" className={styles.btnPastInformation}>過去の情報</a>
-  
+        <div className={styles.container}>
+        <div className={styles.buttons}>
+
+          <div className={styles.empty}></div>
+          
+          <div className={styles.btnbox}>
+            <Link href="/Information" className={styles.btn}>
+              <div class={styles.btnname}>　情報　</div>
+              <div class={styles.btncolor}></div>
+            </Link>
+          </div>
+
+          <div className={styles.btnbox}>
+            <Link href="/Serch" className={styles.selectedbtn}>
+              <div class={styles.btnname}>　検索　</div>
+              <div class={styles.btncolor}></div>
+            </Link>
+          </div>
+          <div className={styles.btnbox}>
+            <Link href="/Relations" className={styles.btn}>
+              <div class={styles.btnname}>　関係図　</div>
+              <div class={styles.btncolor}></div>
+            </Link>
+          </div>
+          <div className={styles.btnbox}>
+            <Link href="/Missions" className={styles.btn}>
+              <div class={styles.btnname}>　ミッション　</div>
+              <div class={styles.btncolor}></div>
+            </Link>
+          </div>
+          <div className={styles.btnbox}>
+            <Link href="/PastInformation" className={styles.btn}>
+              <div class={styles.btnname}>　過去の情報　</div>
+              <div class={styles.btncolor}></div>
+            </Link>
+          </div>
+        </div>
+        <div id="loading">📱 ブラウザのカメラの使用を許可してください。</div>
+            <button id = "camerabutton"onClick={a} className = {styles.Camerabtn}>カメラを起動する</button>
+            <canvas id="canvas" className = {styles.canvas} hidden></canvas>
+            <p id="qr-msg"　className={styles.qrmsg}></p>
+       
+         
+              
+      </div>
             <div >
 
-                <input  className={styles.search_bar} id="SearchBox" value = {text}  onChange={(event) => setText(event.target.value)} type="text" placeholder="キーワードを入力"></input>
+                <input  className={styles.search_bar} id="SearchBox"  type="text" placeholder="キーワードを入力"></input>
                 
-
+                <p id  = "SearchResult" className = {styles.SearchReturn} >tinntinn</p>
                 
                
                 <input className = {styles.search_submit} type="button" value="検索" onClick={OnSearch} /><br />
-                <br />
-                <br />
-                <br />
-                <p>{ReturnWord1}</p>
                 
+                
+            
+
             </div>
+            
+       
+          
+           
         
    
         
       
       </div>
     
+    
         
         
       
     );
+   
   }
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-        </Head>
+        <Head />
         <body>
           <Main />
           <div id="portal"></div>
