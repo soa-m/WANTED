46652462e78c6bffde3d83bbe4fdd612c2250a4a
kozy-koz <148466472+kozy-koz@users.noreply.github.com>
2023-11-02 06:29:34 +0900

    Update Relations.js

--- pages/Relations.js
+++ pages/Relations.js
@@ -3,21 +3,42 @@ import styles from '../styles/Rel.module.css';
 import Image from "next/image";
 import { useEffect } from 'react'
 
+var name=["星野初翔","星野世貴","星野徹子","猫田忠文","X","猫田真弥華"]
+var charatext=["10/30に失踪した高校二年生の男子。ネグレクト気味の両親のもとで育てられている。また年々体の動きが鈍くなってるように感じているが、病院に行かせてもらえない。","Atiti","Ahaha","y","x","ytuma"]
 function open1() {
   var x = document.getElementById(styles.Modal1);
   x.style.display = 'block';
 }
-function close1() {
-  var x = document.getElementById(styles.Modal1);
-  x.style.display = 'None';
+function open2() {
+  var x = document.getElementById(styles.Modal2);
+  x.style.display = 'block';
+}
+function open3() {
+  var x = document.getElementById(styles.Modal3);
+  x.style.display = 'block';
+}
+function open4() {
+  var x = document.getElementById(styles.Modal4);
+  x.style.display = 'block';
+}
+function open5() {
+  var x = document.getElementById(styles.Modal5);
+  x.style.display = 'block';
+}
+function open6() {
+  var x = document.getElementById(styles.Modal6);
+  x.style.display = 'block';
 }
 export default function Home() {
   useEffect(() => {
-    const modal = document.getElementById(styles.Modal1);
+    var modals=[document.getElementById(styles.Modal1),document.getElementById(styles.Modal2),document.getElementById(styles.Modal3),document.getElementById(styles.Modal4),document.getElementById(styles.Modal5),document.getElementById(styles.Modal6)]
     window.addEventListener('click', outsideClose);
     function outsideClose(e) {
-      if (e.target == modal) {
-        modal.style.display = 'none';
+      for(let i=0;i<modals.length;i++){
+        let modal=modals[i];
+        if (e.target==modal){
+          modal.style.display = 'none';
+        }
       }
     }
   })
@@ -29,9 +50,7 @@ export default function Home() {
           <div className={styles.empty}></div>
 
           <div className={styles.btnbox}>
-            <Link href="/Information" className={styles.btn}>
-              <div className={styles.btnname}>　情報　</div>
-              <div className={styles.btncolor}></div>
+            <Link href="/Information" className={styles.btn} id={styles.story}>
             </Link>
           </div>
 
@@ -42,9 +61,7 @@ export default function Home() {
             </Link>
           </div>
           <div className={styles.btnbox}>
-            <Link href="/Relations" className={styles.selectedbtn}>
-              <div className={styles.btnname}>　関係図　</div>
-              <div className={styles.btncolor}></div>
+            <Link href="/Relations" className={styles.selectedbtn} id={styles.connection}>
             </Link>
           </div>
           <div className={styles.btnbox}>
@@ -54,16 +71,18 @@ export default function Home() {
             </Link>
           </div>
           <div className={styles.btnbox}>
-            <Link href="/PastInformation" className={styles.btn}>
-              <div className={styles.btnname}>　過去の情報　</div>
-              <div className={styles.btncolor}></div>
+            <Link href="/item" className={styles.btn} id={styles.item}>
             </Link>
           </div>
         </div>
 
         <div className={styles.main}>
-          <div className={styles.relpic} id={styles.relpic1} onClick={open1}>
-          </div>
+          <div className={styles.relpic} id={styles.ytuma} onClick={open6}></div>
+          <div className={styles.relpic} id={styles.x} onClick={open5}></div>
+          <div className={styles.relpic} id={styles.y} onClick={open4}></div>
+          <div className={styles.relpic} id={styles.Ahaha} onClick={open3}></div>
+          <div className={styles.relpic} id={styles.Atiti} onClick={open2}></div>
+          <div className={styles.relpic} id={styles.Aman} onClick={open1}></div>
         </div>
 
 
@@ -72,16 +91,94 @@ export default function Home() {
       <div id={styles.Modal1} className={styles.modal}>
         <div className={styles.modalcontent}>
           <div className={styles.modalheader}>
-            <span className={styles.modalClose} onClick={close1}>×</span>
           </div>
           <div className={styles.modalbody}>
-            <div className={styles.modalpic}></div>
+            <div id={styles.modalpic1}></div>
             <div className={styles.modalline}></div>
             <div className={styles.modaltext}>
-              <p>主人公君</p>
-              <p>　2006年8月27日生まれ。つくこま高校（土筆学園狛犬高等学校）2年に在籍する。やや長身でやせ型、背丈のわりに手足が長い。塩顔。地学分野（とりわけ鉱石類）への知見が深く、地学部の会計兼主将を務めている。また学年随一の読書家であり、自分で文章を作るのも得意。今年の文化祭でも、文学同好会の会誌へ詩を数篇寄稿していた。</p>
-              <p>　性格は温和で、物静か。基本的に一人で過ごすことを好むタイプだが、真面目で面倒見がいいため周囲からの信頼は厚い。素行・生活態度に目立った点はなし。若干の遅刻癖あり。感情をあまり表に出さないため、いつ見ても冷静でいるような印象を受けるが、親しい友人曰く、実際は感情的になりやすい一面もあるとのこと。クラスの中では「怒らせたらやばそうな人」として認識されている。
-              </p>
+              <div className={styles.modaltextitle}>{name[0]}</div>
+              <br/>
+              <div className={styles.modaltexts}>{charatext[0]}</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div id={styles.Modal2} className={styles.modal}>
+        <div className={styles.modalcontent}>
+          <div className={styles.modalheader}>
+          </div>
+          <div className={styles.modalbody}>
+            <div id={styles.modalpic2}></div>
+            <div className={styles.modalline}></div>
+            <div className={styles.modaltext}>
+              <div className={styles.modaltextitle}>{name[1]}</div>
+              <br/>
+              <div className={styles.modaltexts}>{charatext[1]}</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div id={styles.Modal3} className={styles.modal}>
+        <div className={styles.modalcontent}>
+          <div className={styles.modalheader}>
+          </div>
+          <div className={styles.modalbody}>
+            <div id={styles.modalpic3}></div>
+            <div className={styles.modalline}></div>
+            <div className={styles.modaltext}>
+              <div className={styles.modaltextitle}>{name[2]}</div>
+              <br/>
+              <div className={styles.modaltexts}>{charatext[2]}</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div id={styles.Modal4} className={styles.modal}>
+        <div className={styles.modalcontent}>
+          <div className={styles.modalheader}>
+          </div>
+          <div className={styles.modalbody}>
+            <div id={styles.modalpic4}></div>
+            <div className={styles.modalline}></div>
+            <div className={styles.modaltext}>
+              <div className={styles.modaltextitle}>{name[3]}</div>
+              <br/>
+              <div className={styles.modaltexts}>{charatext[3]}</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div id={styles.Modal5} className={styles.modal}>
+        <div className={styles.modalcontent}>
+          <div className={styles.modalheader}>
+          </div>
+          <div className={styles.modalbody}>
+            <div id={styles.modalpic5}></div>
+            <div className={styles.modalline}></div>
+            <div className={styles.modaltext}>
+              <div className={styles.modaltextitle}>{name[4]}</div>
+              <br/>
+              <div className={styles.modaltexts}>{charatext[4]}</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div id={styles.Modal6} className={styles.modal}>
+        <div className={styles.modalcontent}>
+          <div className={styles.modalheader}>
+          </div>
+          <div className={styles.modalbody}>
+            <div id={styles.modalpic6}></div>
+            <div className={styles.modalline}></div>
+            <div className={styles.modaltext}>
+              <div className={styles.modaltextitle}>{name[5]}</div>
+              <br/>
+              <div className={styles.modaltexts}>{charatext[5]}</div>
             </div>
           </div>
 
