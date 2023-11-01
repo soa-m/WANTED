4ce9755b9023dff4f5ca40bf4d46f6968244e68b
kozy-koz <148466472+kozy-koz@users.noreply.github.com>
2023-11-02 06:29:03 +0900

    Update item.js

--- pages/item.js
+++ pages/item.js
@@ -3,6 +3,16 @@ import styles from '../styles/item.module.css';
 import Image from "next/image";
 import { useEffect } from 'react'
 
+var diarycontent=[
+  "家の蛇口からお湯が出なくなった。どうやらガスを止められたらしい。シャワーはまだ我慢できるけど、料理で水しか使えないのはしんどいな…。まだ寒い季節じゃなかっただけマシ、ってことにしておこう。",
+  "今日は久しぶりに「あの人」のお家へお邪魔した。やっぱり話を聞いて貰える人がいるのはいいね。気分転換になったかな。僕のことを考えて色々な提案をしてくれたのも、本当にありがたかった。けど、今はもう少し自分の力で頑張ってみようかな。本当はもっと長居したかったけど、家族が帰ってきちゃうからって帰された。仕方なし。",
+  "久しぶりに家のポストを見た。役所から届いたであろう、赤い封筒がギッシリ。中身は見ていないけれど、未払いだの滞納だのが沢山あって大変らしい。母さん、自分は贅沢三昧遊び歩いているのに、家のことはいつまでもだらしないんだな。やっぱり、僕が何とかしないとダメみたい。辛い。",
+  "遂に！母さんの旅行が14日目を迎えた。あと二週間耐えれば、母さんがハワイだかグアムだかサイパンだかから帰ってくる。家に帰ってきたら、流石にガスの料金も納めに行くでしょ。そしたら地獄から解放される。先が見えた、気がする。",
+  "今日も父さんは部屋から出てこない。思えば、二人で最後に会話したのって何年前だろう。ここ２年くらいは顔も合わせていない気がする。部屋の前に置いた夕食がなくなっていたから、まだ生きてはいるはず。深夜にもう一度部屋の前を通ったら、空になった食器の傍らに、ティッシュへ書かれたメモが残されていた。「米　冷たくて固い」",
+  "集金の来客があった。電気料金が半年くらい払われていないらしい。催促されたから、事情を話して帰ってもらった。怒られはしなかったけど、憐れむような目で見られるのが一番辛い。母さんが食費として置いていった3000円がもう無くなりそう。10日くらいなら、何も食べなくても凌げるのだろうか…真弥華さんのところへ行ったら、最低限の寝食は担保されるだろう。要検討。",
+  "もういやだ。",
+  "これ以上は我慢できない。前からの計画を実行する。本当は文化祭に参加したかったけど…まずは安全な環境で衣食住を満たしたい。"
+]
 var ismailunlocked=[true, true,true,true];
 
 function open2(){
@@ -29,6 +39,7 @@ function openmails(eve){
   for(let i=0;i<8;i++){
     X[i].style.display="none"
   }
+
   X[eve].style.display='block';
 }
 
@@ -84,16 +95,32 @@ function display(num){
         pic.style.display="block"
     }
     var letterbtn=document.getElementById("letterbtn");
-    if (num==3 && itemfl[num]){
-        letterbtn.style.display="block";
+    if (!itemfl[num]){
+      letterbtn.style.display="none";
     }
     else{
+      console.log(num);
+      if (num==1){
+        letterbtn.onclick=()=>{diary()};
+        console.log(num);
+        letterbtn.style.display="block";
+      }
+      else if (num==3){
+        letterbtn.onclick=()=>{open2();openmails(1);}
+        letterbtn.style.display="block";
+      }
+      else{
+        letterbtn.onClick=()=>{};
         letterbtn.style.display="none";
+      }
     }
     
 }
 function letter(){
-    console.log(1);
+    console.log(11);
+}
+function diary(){
+  open3();
 }
 export default function Home() {
     let imgs=["hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png","hatena.png"]
@@ -195,11 +222,8 @@ export default function Home() {
                     <Image src="" width={300} height={300} id={styles.pic}></Image>
                 </div>
                 <div className={styles.text} id="text"></div>
-                <div className={styles.letterbtn} id="letterbtn">
-                    <button className={styles.letternobtn} onClick={() => {
-          open2();
-          openmails(1);
-        }}>開く</button>
+                <div className={styles.letterbtn} id="letterbtn" onClick={()=>{}}>
+                    <button className={styles.letternobtn}>開く</button>
                 </div>
             </div>
         </div>
@@ -255,6 +279,18 @@ export default function Home() {
           </div>
 
 
+        </div>
+      </div>
+      <div id={styles.Modal3} className={styles.modal}>
+        <div className={styles.modalcontent2}>
+<p>【10月25日】<br/>{diarycontent[0]}</p>
+<p>【10月26日】<br/>{diarycontent[1]}</p>
+<p>【10月27日】<br/>{diarycontent[2]}</p>
+<p>【10月28日】<br/>{diarycontent[3]}</p>
+<p>【10月29日】<br/>{diarycontent[4]}</p>
+<p>【10月30日】<br/>{diarycontent[5]}</p>
+<p>【10月31日】<br/>{diarycontent[6]}</p>
+<p>【11月1日】 <br/>{diarycontent[7]}</p>
         </div>
       </div>
       </>
