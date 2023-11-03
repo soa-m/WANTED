91fba9ec8aaffb968bac9eb3ddbf31242fd6e73f
rukoruko9 <shimotsu96@gmail.com>
2023-11-03 09:19:41 +0900

    aaa

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
       if(!(playerid >0 && playerid <=14)) return;
       else {
         /*Set("PLAYERID",playerid);*/
-        document.location.href = "/firstpage";
+        document.location.href = "/playername";
       }
     }
   return (
